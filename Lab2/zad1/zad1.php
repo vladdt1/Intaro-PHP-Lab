18838c22abfe1c708a95b264c3b52ac5476733e0
Vladislav Titov <100470279+vladdt1@users.noreply.github.com>
2023-03-01 15:05:27 +0300

    Update zad1.php

--- Lab2/zad1/zad1.php
+++ Lab2/zad1/zad1.php
@@ -1,26 +1,34 @@
 <div style="background-color: antiquewhite; margin-left:30%;margin-right: 30%; margin-top:5%">
 <?php
 //Разобьем строку в массив по каждому элементу
-$arr = explode("'", $_GET['text']);
+$arr = str_split($_GET['text']);
 //Циклом прогоняем каждый элемент, прыгаем только по четным элементам массива
-for($i=1;$i<count($arr);$i+=2){
+for($i=0;$i<count($arr);$i++){
+    if(($arr[$i]=="'" and $arr[$i+2]=="'")){
     //Если мы натыкаемся на число в ковычках, то умножаем его на 2 и перезаписываем в массив
-    $arr[$i] = intval($arr[$i]) * 2;
+        $arr[$i+1] = intval($arr[$i+1]) * 2;
+        $i+=2;
+    }else if($arr[$i]=="'" and $arr[$i+3]=="'"){
+        $num = $arr[$i+1].$arr[$i+2];
+        $arr[$i+1] = intval($num) * 2;
+        $arr[$i+2]="";
+        $i+=3;
+    }
 }
 //Создаем строку с результатом
-$res='';
+$arr=implode($arr);
 //Заполним результирующую строку новыми данными из массива
-foreach($arr as $row){
-    $res .= $row . "'";
-}
+// foreach($arr as $row){
+//     $res .= $row . "'";
+// }
 //Удаление последней ковычки, если последний элемент массива был равен пустой
-if($arr[count($arr)-1]){
-    $res = substr($res,0,-1);
-}
+//if($arr[count($arr)-1]){
+//    $res = substr($res,0,-1);
+//}
 //Удаление последней, не нужной ковычки
-$res = substr($res,0,-1);
+//$res = substr($res,0,-1);
 
-echo 'Введенная строка: ' . $_GET['text'] . '<br>' . 'Строка с результатом: ' . $res;
+echo 'Введенная строка: ' . $_GET['text'] . '<br>' . 'Строка с результатом: ' . $arr;
 ?>
 </div>
 <a href="index.php" style="color:white">
